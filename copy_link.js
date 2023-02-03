ead758c141f78fe49bbd417046241be7bcc2dcaa
TomaszDomagalski <97299963+SaltyBoy003@users.noreply.github.com>
2023-02-03 19:00:04 +0100

    Delete copy_link.js

--- copy_link.js
+++
@@ -1,9 +0,0 @@
-    let mail = "w.weronikadomagalska@gmail.com";
-  const copy_link = async () => {
-    try {
-      await navigator.clipboard.writeText(mail);
-      console.log('Mail copied');
-    } catch (err) {
-      console.error('Failed to copy: ', err);
-    }
-  }
